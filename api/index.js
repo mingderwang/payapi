e10569f16defd16d7fa0c3db9bd390eed6586171
Ming-der Wang <mingderwang@gmail.com>
2022-06-15 20:08:20 +0800

    add cors demo https://test1-mintcoin7.vercel.app/api/cors

--- api/index.js
+++ api/index.js
@@ -1,14 +1,16 @@
-const app = require('express')();
-const { v4 } = require('uuid');
+const app = require("express")();
+const { v4 } = require("uuid");
 
-app.get('/api', (req, res) => {
+// Cache-Control refer to https://developer.mozilla.org/zh-TW/docs/Web/HTTP/Headers/Cache-Control
+
+app.get("/api", (req, res) => {
   const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
 
-app.get('/api/item/:slug', (req, res) => {
+app.get("/api/item/:slug", (req, res) => {
   const { slug } = req.params;
   res.end(`Item: ${slug}`);
 });
