c6656392834f0c2f8d192a3fc48d03e540f9c4b0
Ming-der Wang <mingderwang@gmail.com>
2022-06-17 00:08:55 +0800

    ok for auth with github

--- api/index.js
+++ api/index.js
@@ -1,18 +1,32 @@
-const app = require("express")();
-const { v4 } = require("uuid");
+const { send } = require("micro");
+const microAuthGithub = require("microauth-github");
+require("dotenv").config();
 
-// Cache-Control refer to https://developer.mozilla.org/zh-TW/docs/Web/HTTP/Headers/Cache-Control
+const options = {
+  clientId: process.env.GITHUB_ID,
+  clientSecret: process.env.GITHUB_SECRET,
+  callbackUrl: "http://localhost:3000/auth/github/callback",
+  path: "/auth/github",
+  scope: "user",
+};
 
-app.get("/api", (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader("Content-Type", "text/html");
-  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+const githubAuth = microAuthGithub(options);
+
+// third `auth` argument will provide error or result of authentication
+// so it will { err: errorObject } or { result: {
+//  provider: 'github',
+//  accessToken: 'blahblah',
+//  info: userInfo
+// }}
+module.exports = githubAuth(async (req, res, auth) => {
+  if (!auth) {
+    return send(res, 404, "Not Found");
+  }
+
+  if (auth.err) {
+    // Error handler
+    return send(res, 403, "Forbidden");
+  }
+
+  return `Hello ${auth.result.info.login}`;
 });
-
-app.get("/api/item/:slug", (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
-});
-
-module.exports = app;
